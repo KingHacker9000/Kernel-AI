9bda55e152ee09461806bcf2aad9159bcc48265b
Ashish Ajin <68105674+KingHacker9000@users.noreply.github.com>
2025-07-10 13:33:23 +0200

    Configure Java 17 options

--- contextkernel/build.gradle.kts
+++ contextkernel/build.gradle.kts
@@ -11,6 +11,14 @@ android {
         minSdk = 34
         targetSdk = 35
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
