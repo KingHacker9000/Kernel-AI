415c80647ec4512f1141ea7280f811b4bb48f2df
Ashish Ajin <68105674+KingHacker9000@users.noreply.github.com>
2025-07-10 10:04:10 +0200

    Add Android monorepo skeleton

---
+++ kernel-ai-demo/contextkernel/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 34
+        targetSdk = 35
+    }
+}
+
+dependencies {
+    // TODO: add library dependencies
+}
